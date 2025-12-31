17042922b3d221cf9d739177d93fddb525365541
emmanuel4365 <omalearumona@gmail.com>
2025-12-31 13:08:58 +0100

    nodemon and ts-node added and server started

---
+++ frontend/src/components/imageComp.tsx
@@ -0,0 +1,10 @@
+import compImg from '../assets/comp-img.jpg';
+
+const ImageComp = () => {
+    return (
+        <div>
+            <img src={compImg} alt="" />
+        </div>
+    );
+};
+export default ImageComp;
